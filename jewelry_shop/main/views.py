b3db70228d8dd50a1f5e7a1aad235aee26377f1e
GeorgProfi <georgprofi04@gmail.com>
2022-10-09 17:38:56 +0300

    product catalog forms created

--- jewelry_shop/main/views.py
+++ jewelry_shop/main/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Collection
 
-# Create your views here.
+
 def catalog(request):
-    return render(request,'main/index.html')
+    if request.method == 'POST':
+        if 'buy_btn' in request.POST:
+            deleted_instance = Collection.objects.filter(id=request.POST.get('buy_btn'))
+            deleted_instance.delete()
+    Catalog = Collection.objects.all()
+    return render(request, 'main/index.html', {'Catalog': Catalog})
 
 def about(reqest):
     return render(reqest, 'main/about.html')
